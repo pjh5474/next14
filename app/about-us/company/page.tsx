f72c60dd99a6371b2be6df0a36ce857bd51272ca
pjh5474 <warwarsn@gmail.com>
2024-04-18 17:42:10 +0900

    Layouts & Metadata

--- app/about-us/company/page.tsx
+++ app/about-us/company/page.tsx
@@ -1,9 +1,6 @@
-import Navigation from "../../../components/navigation";
-
 export default function Company() {
   return (
     <div>
-      <Navigation />
       <h1>Company !</h1>
     </div>
   );
--- app/about-us/company/sales/page.tsx
+++ app/about-us/company/sales/page.tsx
@@ -1,9 +1,6 @@
-import Navigation from "../../../../components/navigation";
-
 export default function Sales() {
   return (
     <div>
-      <Navigation />
       <h1>Sales team !</h1>
     </div>
   );
