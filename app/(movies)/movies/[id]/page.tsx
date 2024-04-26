f72a09e3feaabcac47a28d25435d312e12166b84
pjh5474 <warwarsn@gmail.com>
2024-04-26 16:57:18 +0900

    Suspense

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,15 +1,20 @@
-import { API_URL } from "../../../api";
-
-async function getMovie(id: string) {
-  const response = await fetch(`${API_URL}/${id}`);
-  return await response.json();
-}
+import { Suspense } from "react";
+import MovieInfo from "../../../../components/movie-info";
+import MovieVideos from "../../../../components/movie-videos";
 
 export default async function MovieDetail({
   params: { id },
 }: {
   params: { id: string };
 }) {
-  const movie = await getMovie(id);
-  return <h1>{movie.title}</h1>;
+  return (
+    <div>
+      <Suspense fallback={<h1>Loading Movie Info</h1>}>
+        <MovieInfo id={id} />
+      </Suspense>
+      <Suspense fallback={<h1>Loading Movie Video</h1>}>
+        <MovieVideos id={id} />
+      </Suspense>
+    </div>
+  );
 }
