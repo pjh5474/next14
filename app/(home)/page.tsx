52eca63904e7e2ab9592d9c3850fbff786d89391
pjh5474 <warwarsn@gmail.com>
2024-04-26 17:32:58 +0900

    Movie Styles

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,4 +1,5 @@
-import Link from "next/link";
+import Movie from "../../components/movie";
+import styles from "../../styles/home.module.css";
 import { API_URL } from "../api";
 
 export const metadata = {
@@ -7,17 +8,22 @@ export const metadata = {
 
 async function getMovies() {
   // await new Promise((resolve) => setTimeout(resolve, 1000));
-  return await fetch(API_URL).then((response) => response.json());
+  const response = await fetch(API_URL);
+  const json = await response.json();
+  return json;
 }
 
 export default async function HomePage() {
   const movies = await getMovies();
   return (
-    <div>
+    <div className={styles.container}>
       {movies.map((movie) => (
-        <li key={movie.id}>
-          <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
-        </li>
+        <Movie
+          key={movie.id}
+          id={movie.id}
+          poster_path={movie.poster_path}
+          title={movie.title}
+        />
       ))}
     </div>
   );
